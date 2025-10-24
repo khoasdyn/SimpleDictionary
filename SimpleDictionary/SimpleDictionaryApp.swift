9e68a6b9aae05ddbaebf17d4e03a2a25df468164
Liam <khoasdyn@gmail.com>
2025-10-24 21:39:30 +0700

    Initial Commit

---
+++ SimpleDictionary/SimpleDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDictionaryApp.swift
+//  SimpleDictionary
+//
+//  Created by khoasdyn on 24/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
